2a0ab70a62fce0ed31f39a12c8b5e929dc7352cd
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-26 16:41:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(window).on('load', function () {
+    let h1 = $('<h1>My restaurant page</h1>')
+    $('#content').append(h1)
+    $('#content').append('<img id="theImg" src="./image1.jpg" />')
+});
+
+
+
